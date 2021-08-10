0e9fd1d1ffc0b1a6e64e46fdd126be7772997db7
Vinicius Nascimento <83601645+viniciusnasc@users.noreply.github.com>
2021-08-09 22:47:05 -0300

    Add project files.

---
+++ CadastroDeSeries/Models/Context.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace CadastroDeSeries.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Serie> Series { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-R9JFMSC\\SQLEXPRESS;Database=CadastroDeSeries;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public virtual void SetModified(object entity)
+        {
+            Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
---
+++ CadastroDeSeries/Models/Serie.cs
@@ -0,0 +1,17 @@
+﻿using CadastroDeSeries.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroDeSeries.Models
+{
+    public class Serie
+    {
+        public int Id { get; set; }
+        public Genero Genero { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public int Ano { get; set; }
+    }
+}
